35c1030b2370cfd2e2e866b75bb00a3af76c81f7
Roman Gasyul <romanslex@gmail.com>
2020-12-19 15:21:08 +0300

    FEATURE: Add logic to hide desktop sidebar on mobiles #38

---
+++ src/features/Sidebar/components/DesktopSidebar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { AppMenu } from 'features/Sidebar/components/AppMenu';
+import Sider from 'antd/es/layout/Sider';
+
+export const DesktopSidebar: React.FC = () => {
+  return (
+    <Sider
+      trigger={null}
+      collapsible
+      theme="dark"
+      width={250}
+      className="desktop-sidebar"
+    >
+      <div
+        className="c-white d-flex d-flex_justify--center d-flex_align--center fs-xl"
+        style={{ height: '64px' }}
+      >
+        GH Todo
+      </div>
+      <AppMenu />
+    </Sider>
+  );
+};
--- src/features/Sidebar/components/Sidebar.tsx
+++ src/features/Sidebar/components/Sidebar.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import Layout from 'antd/es/layout';
-import { AppMenu } from 'features/Sidebar/components/AppMenu';
 import { MobileSidebar } from 'features/Sidebar/components/MobileSidebar';
-
-const { Sider } = Layout;
+import { DesktopSidebar } from 'features/Sidebar/components/DesktopSidebar';
 
 export const Sidebar: React.FC = () => {
   return (
     <>
-      <Sider trigger={null} collapsible theme="dark" width={250}>
-        <div
-          className="c-white d-flex d-flex_justify--center d-flex_align--center fs-xl"
-          style={{ height: '64px' }}
-        >
-          GH Todo
-        </div>
-        <AppMenu />
-      </Sider>
+      <DesktopSidebar />
       <MobileSidebar />
     </>
   );
