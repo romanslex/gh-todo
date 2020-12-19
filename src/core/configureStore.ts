9eba7f8b21630b94ec19a9a5ff99b5105c071650
Roman Gasyul <romanslex@gmail.com>
2020-12-19 13:25:18 +0300

    FEATURE: Add saga

--- src/core/configureStore.ts
+++ src/core/configureStore.ts
@@ -1,8 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { createBrowserHistory } from 'history';
-import { connectRouter } from 'connected-react-router';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+import createSagaMiddleware from 'redux-saga';
+import { all } from 'redux-saga/effects';
 
 export const history = createBrowserHistory();
+const sagaMiddleware = createSagaMiddleware();
+
+function* rootSaga() {
+  yield all([]);
+}
 
 export const store = configureStore({
   reducer: {
@@ -10,4 +17,8 @@ export const store = configureStore({
     // @ts-ignore
     router: connectRouter(history),
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(routerMiddleware(history), sagaMiddleware),
 });
+
+sagaMiddleware.run(rootSaga);
