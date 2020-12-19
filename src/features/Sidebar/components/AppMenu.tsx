18ee6f84aad5d20d551cd42d26dd2d90a40479e3
Roman Gasyul <romanslex@gmail.com>
2020-12-19 18:01:13 +0300

    FEATURE: Add icons #GH-4

--- src/features/Sidebar/components/AppMenu.tsx
+++ src/features/Sidebar/components/AppMenu.tsx
@@ -3,19 +3,22 @@ import Menu from 'antd/es/menu';
 import { Link } from 'react-router-dom';
 import { ERoute } from 'common/const/Router.const';
 import { RouterHooks } from 'common/hooks/Router.hooks';
+import InboxOutlined from '@ant-design/icons/InboxOutlined';
+import CalendarOutlined from '@ant-design/icons/CalendarOutlined';
+import HourglassOutlined from '@ant-design/icons/HourglassOutlined';
 
 export const AppMenu: React.FC = () => {
   const currentPath = RouterHooks.useCurrentPath();
 
   return (
     <Menu theme="dark" mode="inline" selectedKeys={[currentPath]}>
-      <Menu.Item key={ERoute.Inbox}>
+      <Menu.Item key={ERoute.Inbox} icon={<InboxOutlined />}>
         <Link to={ERoute.Inbox}>Inbox</Link>
       </Menu.Item>
-      <Menu.Item key={ERoute.Today}>
+      <Menu.Item key={ERoute.Today} icon={<HourglassOutlined />}>
         <Link to={ERoute.Today}>Today</Link>
       </Menu.Item>
-      <Menu.Item key={ERoute.Week}>
+      <Menu.Item key={ERoute.Week} icon={<CalendarOutlined />}>
         <Link to={ERoute.Week}>Week</Link>
       </Menu.Item>
     </Menu>
