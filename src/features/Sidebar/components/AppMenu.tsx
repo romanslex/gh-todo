d282b01176d347b54d594957ada9b9580d8c1ad5
Roman Gasyul <romanslex@gmail.com>
2020-12-19 18:29:28 +0300

    FEATURE: Add create project button #GH-4

--- src/features/Sidebar/components/AppMenu.tsx
+++ src/features/Sidebar/components/AppMenu.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Menu from 'antd/es/menu';
 import { Link } from 'react-router-dom';
 import { ERoute } from 'common/const/Router.const';
@@ -6,9 +6,13 @@ import { RouterHooks } from 'common/hooks/Router.hooks';
 import InboxOutlined from '@ant-design/icons/InboxOutlined';
 import CalendarOutlined from '@ant-design/icons/CalendarOutlined';
 import HourglassOutlined from '@ant-design/icons/HourglassOutlined';
+import ProjectOutlined from '@ant-design/icons/ProjectOutlined';
+import { SubMenuItemWithAddBtn } from 'features/Sidebar/components/SubMenuItemWithAddBtn';
 
 export const AppMenu: React.FC = () => {
   const currentPath = RouterHooks.useCurrentPath();
+
+  const openEditProjectModal = useCallback(() => {}, []);
 
   return (
     <Menu theme="dark" mode="inline" selectedKeys={[currentPath]}>
@@ -21,7 +25,15 @@ export const AppMenu: React.FC = () => {
       <Menu.Item key={ERoute.Week} icon={<CalendarOutlined />}>
         <Link to={ERoute.Week}>Week</Link>
       </Menu.Item>
-      <Menu.SubMenu title="Projects" />
+      <Menu.SubMenu
+        title={
+          <SubMenuItemWithAddBtn
+            icon={<ProjectOutlined />}
+            onAdd={openEditProjectModal}
+            title="Projects"
+          />
+        }
+      />
       <Menu.SubMenu title="Tags" />
       <Menu.SubMenu title="Filters" />
     </Menu>
