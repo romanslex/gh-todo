621cf423ed0b944c741584c930dbc618ac0d7d04
Roman Gasyul <romanslex@gmail.com>
2020-12-19 14:58:11 +0300

    FEATURE: Add sidebar slice #38

--- src/core/configureStore.ts
+++ src/core/configureStore.ts
@@ -3,6 +3,7 @@ import { createBrowserHistory } from 'history';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
 import createSagaMiddleware from 'redux-saga';
 import { all } from 'redux-saga/effects';
+import { sidebarReducer } from 'features/Sidebar/Sidebar.slice';
 
 export const history = createBrowserHistory();
 const sagaMiddleware = createSagaMiddleware();
@@ -16,6 +17,7 @@ export const store = configureStore({
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     router: connectRouter(history),
+    sidebar: sidebarReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(routerMiddleware(history), sagaMiddleware),
