3a9c36e352cadc7fe410eb5b1da9aa49b24758f1
Roman Gasyul <romanslex@gmail.com>
2020-12-19 14:29:26 +0300

    FEATURE: Make desktop sidebar layout

--- src/common/components/Layouts/Default.tsx
+++ src/common/components/Layouts/Default.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Layout } from 'antd';
+import Layout from 'antd/es/layout';
+import { Sidebar } from 'common/components/Sidebar';
 
 const { Header, Content, Footer } = Layout;
 
 export const Default: React.FC = () => {
   return (
     <Layout className="h-100">
-      {/* <Sidebar /> */}
+      <Sidebar />
       <Layout>
         <Header className="bg-white">Header</Header>
         <Content className="p-7">Content</Content>
