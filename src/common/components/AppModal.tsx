6a6d7679fbf3ea1763c30dc24373ce655082d1af
Roman Gasyul <romanslex@gmail.com>
2020-12-26 18:26:49 +0300

    FEATURE: Add project create form #GH-17

--- src/common/components/AppModal.tsx
+++ src/common/components/AppModal.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import Modal from 'antd/es/modal/Modal';
 import { useMediaPredicate } from 'react-media-hook';
 import { MEDIA_MOBILE_MAX_WIDTH } from 'common/const/Common.const';
 import Drawer from 'antd/es/drawer';
 
 interface IComponentProps {
+  title: string;
   isOpen: boolean;
   close: () => void;
 }
 
-export const AppModal: React.FC<IComponentProps> = ({
+type AllProps = PropsWithChildren<IComponentProps>;
+
+export const AppModal: React.FC<AllProps> = ({
+  title,
   isOpen,
   close,
-}: IComponentProps) => {
-  const content = 'Hello from modal';
+  children,
+}: AllProps) => {
   const isMobile: boolean = useMediaPredicate(MEDIA_MOBILE_MAX_WIDTH);
 
   if (isMobile) {
     return (
       <Drawer
+        title={title}
         placement="bottom"
         closable={false}
         visible={isOpen}
@@ -27,20 +32,21 @@ export const AppModal: React.FC<IComponentProps> = ({
         className="modal-content modal-content_mobile"
         height="auto"
       >
-        {content}
+        {children}
       </Drawer>
     );
   }
 
   return (
     <Modal
+      title={title}
       className="modal-content modal-content_desktop"
       visible={isOpen}
       footer={null}
       width={400}
       onCancel={close}
     >
-      {content}
+      {children}
     </Modal>
   );
 };
--- src/features/Projects/components/EditProjectModal.tsx
+++ src/features/Projects/components/EditProjectModal.tsx
@@ -3,6 +3,9 @@ import { AppModal } from 'common/components/AppModal';
 import { useDispatch, useSelector } from 'react-redux';
 import { projectsSelectors } from 'features/Projects/Projects.selectors';
 import { projectsActions } from 'features/Projects/Projects.slice';
+import Form from 'antd/es/form';
+import Input from 'antd/es/input';
+import Button from 'antd/es/button';
 
 export const EditProjectModal: React.FC = () => {
   const dispatch = useDispatch();
@@ -10,5 +13,22 @@ export const EditProjectModal: React.FC = () => {
 
   const close = () => dispatch(projectsActions.toggleEditModal(false));
 
-  return <AppModal isOpen={isOpen} close={close} />;
+  return (
+    <AppModal title="Create new project" isOpen={isOpen} close={close}>
+      <Form layout="vertical">
+        <Form.Item label="Name">
+          <Input />
+        </Form.Item>
+        <Form.Item />
+        <div className="d-flex d-flex_justify--end">
+          <Form.Item className="mr-2">
+            <Button type="primary">Save</Button>
+          </Form.Item>
+          <Form.Item>
+            <Button>Cancel</Button>
+          </Form.Item>
+        </div>
+      </Form>
+    </AppModal>
+  );
 };
