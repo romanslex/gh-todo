9d5a17957aadfbf196956815892453d91fbf4779
Roman Gasyul <romanslex@gmail.com>
2021-02-21 14:15:20 +0300

    FEATURE: Change task layout #GH-32

--- src/common/Helpers/Date.helpers.ts
+++ src/common/Helpers/Date.helpers.ts
@@ -1,7 +1,10 @@
-import { Moment } from 'moment';
+import moment, { Moment } from 'moment';
 
 export const DATE_DISPLAY_FORMAT = 'DD.MM.YYYY';
 
 export const momentToNumber = (date?: Moment): number | undefined => {
   return date?.unix();
 };
+
+export const formatDateNumber = (date: number): string =>
+  moment.unix(date).format(DATE_DISPLAY_FORMAT);
--- src/features/Tasks/components/TaskItem.tsx
+++ src/features/Tasks/components/TaskItem.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import { ITaskModel } from 'features/Tasks/Tasks.models';
 import { Col, Row } from 'antd/es/grid';
 import Checkbox from 'antd/es/checkbox/Checkbox';
 import { ColorCircle } from 'common/components/ColorCircle';
+import { formatDateNumber } from 'common/Helpers/Date.helpers';
+import CalendarOutlined from '@ant-design/icons/CalendarOutlined';
+import Card from 'antd/es/card';
+
+const cardBodyStyles: CSSProperties = {
+  padding: '5px 10px',
+};
 
 interface IComponentProps {
   task: ITaskModel;
@@ -11,18 +18,51 @@ interface IComponentProps {
 export const TaskItem: React.FC<IComponentProps> = ({
   task,
 }: IComponentProps) => {
-  const { name, project } = task;
+  const { name, project, dueDate, tags } = task;
 
   return (
-    <Row gutter={[12, 0]}>
-      <Col>
-        <Checkbox />
-      </Col>
-      <Col flex={1}>{name}</Col>
-      <Col>
-        {project.name}{' '}
-        <ColorCircle width="10px" height="10px" color={project.color} />
-      </Col>
-    </Row>
+    <Card bodyStyle={cardBodyStyles} className="mb-3">
+      <Row gutter={[12, 0]}>
+        <Col>
+          <Checkbox />
+        </Col>
+        <Col flex={1}>
+          <Row>
+            <Col flex={1}>{name}</Col>
+            <Col>
+              <Row align="middle" gutter={[6, 0]}>
+                <Col className="fs-xs">{project.name}</Col>
+                <Col>
+                  <ColorCircle
+                    width="10px"
+                    height="10px"
+                    color={project.color}
+                  />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              {dueDate && (
+                <>
+                  <CalendarOutlined className="mr-1 c-danger" />
+                  <span className="mr-2 fs-xs c-danger">
+                    {formatDateNumber(dueDate)}
+                  </span>
+                </>
+              )}
+            </Col>
+            <Col>
+              {tags?.map((tag) => (
+                <span className="mr-1 fs-xs" key={tag.id}>
+                  {tag.name}
+                </span>
+              ))}
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Card>
   );
 };
