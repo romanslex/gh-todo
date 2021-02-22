d747930274920f522af2b5efea1af59c9eeec6bc
Roman Gasyul <romanslex@gmail.com>
2021-02-22 15:37:38 +0300

    FEATURE: Remake EditTask modal #GH-35

--- src/features/Tasks/components/EditTaskForm.tsx
+++ src/features/Tasks/components/EditTaskForm.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import Form from 'antd/es/form';
 import Input from 'antd/es/input';
-import { Col, Row } from 'antd/es/grid';
 import DatePicker from 'antd/es/date-picker';
 import { useDispatch, useSelector } from 'react-redux';
 import { projectsSelectors } from 'features/Projects/Projects.selectors';
 import Select from 'antd/es/select';
 import { tagsSelectors } from 'features/Tags/Tags.selectors';
-import Card from 'antd/es/card';
 import Button from 'antd/es/button';
 import { Moment } from 'moment';
 import { tasksActions } from 'features/Tasks/Tasks.slice';
 import { DATE_DISPLAY_FORMAT, DateHelper } from 'common/Helpers/Date.helpers';
 import { tasksSelectors } from 'features/Tasks/Tasks.selectors';
+import { AppModal } from 'common/components/AppModal';
+import { Col, Row } from 'antd/es/grid';
 
 interface IFormValues {
   name: string;
@@ -41,12 +41,12 @@ export const EditTaskForm: React.FC = () => {
     dispatch(tasksActions.toggleEditForm({ isOpen: false }));
   };
 
-  if (!isOpen) {
-    return null;
-  }
-
   return (
-    <Card>
+    <AppModal
+      title="Create task"
+      isOpen={isOpen}
+      close={() => tasksActions.toggleEditForm({ isOpen: false })}
+    >
       <Form layout="vertical" autoComplete="off" onFinish={submit}>
         <Form.Item
           name="name"
@@ -55,48 +55,40 @@ export const EditTaskForm: React.FC = () => {
         >
           <Input name="name" placeholder="Todo ..." />
         </Form.Item>
-        <Row gutter={[24, 0]}>
-          <Col span={24} md={6}>
-            <Form.Item name="dueDate" label="Due date">
-              <DatePicker
-                format={DATE_DISPLAY_FORMAT}
-                name="dueDate"
-                className="w-100"
-              />
-            </Form.Item>
-          </Col>
-          <Col span={24} md={6}>
-            <Form.Item
-              name="project"
-              label="Project"
-              rules={[{ required: true, message: 'Field is required' }]}
-            >
-              <Select
-                showSearch
-                className="w-100"
-                placeholder="Select project"
-                optionFilterProp="children"
-              >
-                {projects.map((project) => (
-                  <Select.Option key={project.id} value={project.id}>
-                    {project.name}
-                  </Select.Option>
-                ))}
-              </Select>
-            </Form.Item>
-          </Col>
-          <Col span={24} md={12}>
-            <Form.Item name="tags" label="Tags">
-              <Select mode="multiple" className="w-100" placeholder="Tags">
-                {tags.map((tag) => (
-                  <Select.Option key={tag.id} value={tag.id}>
-                    {tag.name}
-                  </Select.Option>
-                ))}
-              </Select>
-            </Form.Item>
-          </Col>
-        </Row>
+        <Form.Item name="dueDate" label="Due date">
+          <DatePicker
+            format={DATE_DISPLAY_FORMAT}
+            name="dueDate"
+            className="w-100"
+          />
+        </Form.Item>
+        <Form.Item
+          name="project"
+          label="Project"
+          rules={[{ required: true, message: 'Field is required' }]}
+        >
+          <Select
+            showSearch
+            className="w-100"
+            placeholder="Select project"
+            optionFilterProp="children"
+          >
+            {projects.map((project) => (
+              <Select.Option key={project.id} value={project.id}>
+                {project.name}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item name="tags" label="Tags">
+          <Select mode="multiple" className="w-100" placeholder="Tags">
+            {tags.map((tag) => (
+              <Select.Option key={tag.id} value={tag.id}>
+                {tag.name}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
         <Row justify="end">
           <Col>
             <Button type="primary" htmlType="submit">
@@ -108,6 +100,6 @@ export const EditTaskForm: React.FC = () => {
           </Col>
         </Row>
       </Form>
-    </Card>
+    </AppModal>
   );
 };
